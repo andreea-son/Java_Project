4f75123907a2a212150eac785c1fd0fdf516902f
andreea-son <82153848+andreea-son@users.noreply.github.com>
2022-04-11 03:47:44 +0300

    Update Invoice.java

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,8 +1,8 @@
 public class Invoice{
     private int invoiceId;
     private String invoiceDate;
-    private LentBooks returnedBook = new LentBooks();
-    private Users userThatLent = new Users();
+    private LentBooks returnedBook;
+    private Users userThatLent;
     private String cardOrCash;
     public Invoice(int invoiceId, String invoiceDate, LentBooks returnedBook, Users userThatLent, String cardOrCash){
         this.invoiceId = invoiceId;
@@ -12,14 +12,10 @@ public class Invoice{
         this.cardOrCash = cardOrCash;
     }
 
-    public Invoice(){
-
-    }
-
     public void print(){
-        System.out.println("invoice id: " + invoiceId);
+        System.out.println("invoice ID: " + invoiceId);
         System.out.println("lent book: ");
-        System.out.println("    book id: " + returnedBook.getBookId());
+        System.out.println("    book ID: " + returnedBook.getBookId());
         System.out.println("    author: " + returnedBook.getAuthor());
         System.out.println("    title: " + returnedBook.getTitle());
         System.out.println("    description: " + returnedBook.getDescription());
@@ -28,7 +24,7 @@ public class Invoice{
             System.out.println("    return date: " + returnedBook.getReturnDate() + " + exceeded with " + returnedBook.getExceededDays() + " days");
             System.out.println("    additional price: 3 x " + returnedBook.getExceededDays() + " = " + returnedBook.getExceededPrice() + " ron");
             System.out.println("user that lent the book: ");
-            System.out.println("    user id: " + userThatLent.getUserId());
+            System.out.println("    user ID: " + userThatLent.getUserId());
             System.out.println("    the name of the user: " + userThatLent.getUserName());
             System.out.println("    payed with: " + cardOrCash);
         }
@@ -36,7 +32,7 @@ public class Invoice{
             System.out.println("    return date: " + returnedBook.getReturnDate());
             System.out.println("    price: 2 x " + returnedBook.getDaysFromIssue() + " = " + returnedBook.getPrice() + " ron");
             System.out.println("user that lent the book: ");
-            System.out.println("    user id: " + userThatLent.getUserId());
+            System.out.println("    user ID: " + userThatLent.getUserId());
             System.out.println("    the name of the user: " + userThatLent.getUserName());
             System.out.println("    payed with: " + cardOrCash);
             System.out.println("Note: For every day that exceeds the return day you have to pay an additional 3 ron");

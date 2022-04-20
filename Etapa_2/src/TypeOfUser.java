833347f15e47d5a487bf43dc1d497f7aea76d29b
andreea-son <82153848+andreea-son@users.noreply.github.com>
2022-04-21 01:14:04 +0300

    Update TypeOfUser.java

--- Etapa_2/src/TypeOfUser.java
+++ Etapa_2/src/TypeOfUser.java
@@ -3,12 +3,14 @@ abstract class TypeOfUser {
     private String password;
     private String email;
     private int id;
+    private boolean isDeleted;
 
-    public TypeOfUser(int id, String name, String password, String email){
+    public TypeOfUser(int id, String name, String password, String email, boolean isDeleted){
         this.name = name;
         this.email = email;
         this.password = password;
         this.id = id;
+        this.isDeleted = isDeleted;
     }
 
     public TypeOfUser(){
@@ -35,8 +37,12 @@ abstract class TypeOfUser {
         this.password = password;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
     }
 
     public abstract void print();

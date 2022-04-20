7e79f7c909b4896c7c2fc5b080e6b06c589d770f
andreea-son <82153848+andreea-son@users.noreply.github.com>
2022-04-21 01:01:15 +0300

    Update CSVInvoiceService.java

--- Etapa_2/src/CSVInvoiceService.java
+++ Etapa_2/src/CSVInvoiceService.java
@@ -9,31 +9,12 @@ public class CSVInvoiceService {
     private String lineToRead;
     private ArrayList<String> listInvoices1 = new ArrayList<>();
     private ArrayList<String> listInvoices2 = new ArrayList<>();
-    private ArrayList<ArrayList<String>> listInvoiceBook = new ArrayList<>(100);
-    private ArrayList<ArrayList<String>> listInvoiceUser = new ArrayList<>(100);
     private static String[][] invoiceField = new String[100][];
-    private static String[][] invoiceBookField = new String[100][];
-    private static String[][] invoiceUserField = new String[100][];
     private static Integer[] invoiceId = new Integer[100];
     private static String[] invoiceDate = new String[100];
     private static String[] cardOrCash = new String[100];
-    private static String[] invoiceBookPath = new String[100];
-    private static String[] invoiceUserPath = new String[100];
-    private static Integer[] bookId = new Integer[100];
-    private static boolean[] isDeleted = new boolean[100];
-    private static String[] author = new String[100];
-    private static String[] title = new String[100];
-    private static String[] description = new String[100];
-    private static String[] issuedDate = new String[100];
-    private static Integer[] daysFromIssue = new Integer[100];
-    private static boolean[] isLent = new boolean[100];
     private static Integer[] userId = new Integer[100];
-    private static Integer[] numOfLogins = new Integer[100];
-    private static String[] userName = new String[100];
-    private static String[] userPassword = new String[100];
-    private static String[] userEmail = new String[100];
-    private static ArrayList<Users> userThatLent = new ArrayList<>();
-    private static ArrayList<LentBooks> book = new ArrayList<>();
+    private static LibrarianService librarian = new LibrarianService();
 
     private static CSVInvoiceService csvInvoiceService = new CSVInvoiceService();
 
@@ -75,105 +56,13 @@ public class CSVInvoiceService {
             invoiceDate[i] = invoiceField[i][1];
 
         for (int i = 0; i < listInvoices2.size(); i++)
-            invoiceBookPath[i] = invoiceField[i][2];
+            userId[i] = Integer.parseInt(invoiceField[i][2]);
 
         for (int i = 0; i < listInvoices2.size(); i++)
-            invoiceUserPath[i] = invoiceField[i][3];
-
-        for (int i = 0; i < listInvoices2.size(); i++)
-            cardOrCash[i] = invoiceField[i][4];
-
-    }
-
-    protected void readInvoiceBook(){
-        for (int i = 0; i < 100; i++)
-            invoiceBookField[i] = new String[20];
-
-        for (int i = 0; i < 100; i++)
-            listInvoiceBook.add(new ArrayList<>());
+            cardOrCash[i] = invoiceField[i][3];
 
         for (int i = 0; i < listInvoices2.size(); i++) {
-            CSVPath = invoiceBookPath[i];
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(CSVPath));
-                while ((lineToRead = bufferedReader.readLine()) != null) {
-                    listInvoiceBook.get(i).add(lineToRead);
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            invoiceBookField[i] = listInvoiceBook.get(i).get(1).split("\\s*,\\s*");
-
-            bookId[i] = Integer.parseInt(invoiceBookField[i][0]);
-
-            isDeleted[i] = Boolean.parseBoolean(invoiceBookField[i][1]);
-
-            author[i] = invoiceBookField[i][2];
-
-            title[i] = invoiceBookField[i][3];
-
-            description[i] = invoiceBookField[i][4];
-
-            issuedDate[i] = invoiceBookField[i][5];
-
-            daysFromIssue[i] = Integer.parseInt(invoiceBookField[i][6]);
-
-            isLent[i] = Boolean.parseBoolean(invoiceBookField[i][7]);
-        }
-    }
-
-    protected void readInvoiceUser() {
-        for (int i = 0; i < 100; i++)
-            invoiceUserField[i] = new String[20];
-
-        for (int i = 0; i < 100; i++)
-            listInvoiceUser.add(new ArrayList<>());
-
-        for (int i = 0; i < listInvoices2.size(); i++) {
-            CSVPath = invoiceUserPath[i];
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(CSVPath));
-                while ((lineToRead = bufferedReader.readLine()) != null) {
-                    listInvoiceUser.get(i).add(lineToRead);
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            invoiceUserField[i] = listInvoiceUser.get(i).get(1).split(",");
-
-            userId[i] = Integer.parseInt(invoiceUserField[i][0]);
-
-            numOfLogins[i] = Integer.parseInt(invoiceUserField[i][1]);
-
-            userName[i] = invoiceUserField[i][2];
-
-            userPassword[i] = invoiceUserField[i][3];
-
-            userEmail[i] = invoiceUserField[i][4];
-        }
-
-        for (int i = 0; i < listInvoices2.size(); i++) {
-            for (int j = 0; j < LibrarianService.getUsers().size(); j++)
-                if (userId[i] == LibrarianService.getUsers().get(j).getUserId())
-                    userThatLent.add(LibrarianService.getUsers().get(j));
-        }
-
-        for (int i = 0; i < listInvoices2.size(); i++){
-            for (int j = 0; j < LibrarianService.getUsers().size(); j++)
-                for (int k = 0; k < LibrarianService.getUsers().get(j).getUserLentBooks().size(); k++)
-                    if (bookId[i] == LibrarianService.getUsers().get(j).getUserLentBooks().get(k).getBookId()) {
-                        book.add(LibrarianService.getUsers().get(j).getUserLentBooks().get(k));
-                    }
-        }
-
-        for (int i = 0; i < listInvoices2.size(); i++) {
-            LibrarianService.getInvoice().add(new Invoice(invoiceId[i], invoiceDate[i], book.get(i), userThatLent.get(i), cardOrCash[i]));
+            librarian.getInvoice().add(new Invoice(invoiceId[i], invoiceDate[i], userId[i], cardOrCash[i]));
         }
     }
 }

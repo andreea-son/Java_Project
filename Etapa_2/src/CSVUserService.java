dfbb12be4394c856e53a40064bc4979e3624316d
andreea-son <82153848+andreea-son@users.noreply.github.com>
2022-04-21 01:04:19 +0300

    Update CSVUserService.java

--- Etapa_2/src/CSVUserService.java
+++ Etapa_2/src/CSVUserService.java
@@ -10,23 +10,13 @@ public class CSVUserService {
     private String lineToRead;
     private ArrayList<String> listUsers1 = new ArrayList<>();
     private ArrayList<String> listUsers2 = new ArrayList<>();
-    private ArrayList<ArrayList<String>> listUserBooks = new ArrayList<>(100);
     private static String[][] userField = new String[100][];
-    private static String[][] userBooksField = new String[100][];
     private static Integer[] userId = new Integer[100];
     private static Integer[] numOfLogins = new Integer[100];
     private static String[] userName = new String[100];
     private static String[] userPassword = new String[100];
     private static String[] userEmail = new String[100];
-    private static String[] userBooksPath = new String[100];
-    private static Integer[][] bookId = new Integer[100][];
-    private static boolean[][] isDeleted = new boolean[100][];
-    private static String[][] author = new String[100][];
-    private static String[][] title = new String[100][];
-    private static String[][] description = new String[100][];
-    private static String[][] issuedDate = new String[100][];
-    private static Integer[][] daysFromIssue = new Integer[100][];
-    private static boolean[][] isLent = new boolean[100][];
+    private static boolean[] isDeleted = new boolean[100];
 
     private static CSVUserService csvUserService = new CSVUserService();
 
@@ -76,97 +66,9 @@ public class CSVUserService {
             userEmail[i] = userField[i][4];
 
         for (int i = 0; i < listUsers2.size(); i++)
-            if (!userField[i][5].equals("null"))
-                userBooksPath[i] = userField[i][5];
-            else {
-                userBooksPath[i] = "";
-            }
+            isDeleted[i] = Boolean.parseBoolean(userField[i][4]);
+
         for (int i = 0; i < listUsers2.size(); i++)
-            LibrarianService.getUsers().add(new Users(userId[i], numOfLogins[i], userName[i], userPassword[i], userEmail[i]));
-    }
-
-    public void readUserBooks() {
-        for (int i = 0; i < 100; i++)
-            userBooksField[i] = new String[20];
-
-        for (int i = 0; i < 100; i++)
-            listUserBooks.add(new ArrayList<>());
-
-        for (int i = 0; i < 100; i++)
-            bookId[i] = new Integer[20];
-
-        for (int i = 0; i < 100; i++)
-            isDeleted[i] = new boolean[20];
-
-        for (int i = 0; i < 100; i++)
-            author[i] = new String[20];
-
-        for (int i = 0; i < 100; i++)
-            title[i] = new String[20];
-
-        for (int i = 0; i < 100; i++)
-            description[i] = new String[20];
-
-        for (int i = 0; i < 100; i++)
-            issuedDate[i] = new String[20];
-
-        for (int i = 0; i < 100; i++)
-            daysFromIssue[i] = new Integer[20];
-
-        for (int i = 0; i < 100; i++)
-            isLent[i] = new boolean[20];
-
-        for (int i = 0; i < listUsers2.size(); i++) {
-            if (!userBooksPath[i].isEmpty()) {
-                CSVPath = userBooksPath[i];
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new FileReader(CSVPath));
-                    while ((lineToRead = bufferedReader.readLine()) != null) {
-                        listUserBooks.get(i).add(lineToRead);
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    userBooksField[j - 1] = listUserBooks.get(i).get(j).split("\\s*,\\s*");
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    bookId[i][j - 1] = Integer.parseInt(userBooksField[j - 1][0]);
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    isDeleted[i][j - 1] = Boolean.parseBoolean(userBooksField[j - 1][1]);
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    author[i][j - 1] = userBooksField[j - 1][2];
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    title[i][j - 1] = userBooksField[j - 1][3];
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    description[i][j - 1] = userBooksField[j - 1][4];
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    issuedDate[i][j - 1] = userBooksField[j - 1][5];
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    daysFromIssue[i][j - 1] = Integer.parseInt(userBooksField[j - 1][6]);
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    isLent[i][j - 1] = Boolean.parseBoolean(userBooksField[j - 1][7]);
-
-                for (int j = 1; j < listUserBooks.get(i).size(); j++)
-                    LibrarianService.getUsers().get(i).getUserLentBooks().add(new LentBooks(bookId[i][j - 1], isDeleted[i][j - 1], author[i][j - 1], title[i][j - 1], description[i][j - 1], issuedDate[i][j - 1], daysFromIssue[i][j - 1], isLent[i][j - 1]));
-
-            }
-        }
-
-        for (int j = 0; j < LibrarianService.getUsers().size(); j++)
-            LibrarianService.getUserLentBooks().get(j).addAll(LibrarianService.getUsers().get(j).getUserLentBooks());
-
-        for (int j = 0; j < LibrarianService.getUsers().size(); j++)
-            LibrarianService.getUsers().set(j, new Users(userId[j], numOfLogins[j], userName[j], userPassword[j], userEmail[j], LibrarianService.getUsers().get(j).getUserLentBooks()));
+            LibrarianService.getUsers().add(new Users(userId[i], numOfLogins[i], userName[i], userPassword[i], userEmail[i], isDeleted[i]));
     }
 }
